43a1ced93ac5e8cffa9ecd59d718ece4a224c6e7
Noah <noahlezama2000@gmail.com>
2025-10-07 18:09:40 +0200

    feat: add additional screens to navigation stack in App component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import NavBar from "./components/NavBar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Details from "./screens/VaultCardDetails";
+import { Favorites } from "./screens/Favorites";
+import Settings from "./screens/Settings";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +18,9 @@ export default function App() {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Vault" component={Vault} />
           <Stack.Screen name="CreateVault" component={CreateVaultCard} />
+          <Stack.Screen name="VaultCardDetails" component={Details} />
+          <Stack.Screen name="Favorites" component={Favorites} />
+          <Stack.Screen name="Settings" component={Settings} />
         </Stack.Navigator>
         <View
           style={{
